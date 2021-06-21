5fb412b7a7abbd484df3187911f9407057a3feca
ahelmoselhy <a.elmoselhy@up-clicks.com>
2021-06-21 16:55:36 +0200

    #Initial commit

---
+++ lib/ui/my_app.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:world_time/ui/screens/choose_location.dart';
+import 'package:world_time/ui/screens/home.dart';
+import 'package:world_time/ui/screens/loading.dart';
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: "/home",
+      routes: {
+        "/":(context)=>Loading(),
+        "/home":(context)=>Home(),
+        "/choose_location":(context)=>ChooseLocation(),
+      },
+    );
+  }
+
+}
---
+++ lib/ui/screens/home.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: TextButton.icon(
+            onPressed: () {
+              Navigator.pushNamed(context, "/choose_location");
+            },
+            icon: Icon(Icons.add_location,color: Colors.grey[600]),
+            label: Text("Home", style: TextStyle(color: Colors.grey[600])),
+          ),
+        ),
+      ),
+    );
+  }
+}
